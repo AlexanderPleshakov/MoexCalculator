8f18151994751f869c0df4a0d7de15d51b61712b
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-06-11 18:00:14 +0300

    Initial Commit

---
+++ MoexCalculator/MoexCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoexCalculatorApp.swift
+//  MoexCalculator
+//
+//  Created by Александр Плешаков on 11.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoexCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
